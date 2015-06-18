3627dbf5370bb458d1a9b18026b243b6e2e5be0e
Daniel Caaz <DCaazy@gmail.com>
2015-06-17 23:11:56 -0700

    Spaces.

--- core/src/io/thefusion/puzzleattack/Tile.java
+++ core/src/io/thefusion/puzzleattack/Tile.java
@@ -1,26 +1,26 @@
 package io.thefusion.puzzleattack;
 
 public class Tile {
-	public int type = 0;
-	public boolean active = false;
-	public boolean matched = false;
-	public float cooldown = 0f;
-	public float flash = 0f;
+  public int type = 0;
+  public boolean active = false;
+  public boolean matched = false;
+  public float cooldown = 0f;
+  public float flash = 0f;
 
-	public Tile() {
-	}
+  public Tile() {
+  }
 
-	public Tile(boolean activate) {
-		if (activate) {
-			active = true;
-			type = (int) (Math.random() * 6) + 1;
-		}
-	}
+  public Tile(boolean activate) {
+    if (activate) {
+      active = true;
+      type = (int) (Math.random() * 6) + 1;
+    }
+  }
 
-	public void match() {
-		if(!matched) {
-			matched = true;
-			flash = 1;
-		}
-	}
+  public void match() {
+    if (!matched) {
+      matched = true;
+      flash = 1;
+    }
+  }
 }
